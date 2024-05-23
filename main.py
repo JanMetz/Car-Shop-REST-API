21c114d125cb701ed95a8a55597e2d697cb5612f
JanMetz <metzler.infa@gmail.com>
2024-05-23 15:50:50 +0200

    added locks to non-concurrent-safe operations

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import uuid
 import hashlib
-from typing import Dict, Any
+from typing import Any
 
 from resources import *
 from fastapi import FastAPI
@@ -22,8 +22,13 @@ def get_token(resource: Resource) -> str:
 
     while True:
         token = str(uuid.uuid4().hex[:5])
-        not_in_keys = token not in vehicles.entities.keys() and token not in mechanics.entities.keys() and token not in apps.entities.keys()
-        not_in_tokens = token not in vehicles.new_tokens and token not in mechanics.new_tokens and token not in apps.new_tokens
+        not_in_keys = (token not in vehicles.entities.keys() and
+                       token not in mechanics.entities.keys() and
+                       token not in apps.entities.keys())
+
+        not_in_tokens = (token not in vehicles.new_tokens and
+                         token not in mechanics.new_tokens and
+                         token not in apps.new_tokens)
 
         if not_in_tokens and not_in_keys:
             resource.new_tokens.append(token)
@@ -66,9 +71,10 @@ async def post_mechanics():
 
 @app.post("/transfers")
 async def post_transfer(transfer: Transfer):
-    for appointment in apps.entities.values():
-        if appointment.date == transfer.date_from:
-            appointment.date = transfer.date_to
+    async with apps.mutex:
+        for appointment in apps.entities.values():
+            if appointment.date == transfer.date_from:
+                appointment.date = transfer.date_to
 
     return {
         "message": "appointments rescheduling successful",
@@ -78,17 +84,18 @@ async def post_transfer(transfer: Transfer):
 
 # *************** PUTS ***************
 
-def update_collection(entity_id: str, entity, resource: Resource) -> str:
+async def update_collection(entity_id: str, entity, resource: Resource) -> str:
     if entity_id in resource.new_tokens:
         resource.new_tokens.remove(entity_id)
         resource.entities[entity_id] = entity
 
         return f"created entity {entity_id}"
 
-    elif entity_id in resource.entities:
-        resource.entities[entity_id] = entity
+    async with resource.mutex:
+        if entity_id in resource.entities:
+            resource.entities[entity_id] = entity
 
-        return f"updated entity {entity_id}"
+            return f"updated entity {entity_id}"
 
     return f"create/update failed! Token {entity_id} not found"
 
@@ -97,7 +104,7 @@ def update_collection(entity_id: str, entity, resource: Resource) -> str:
 async def put_appointment(app_id: str, appointment: Appointment):
     if appointment.vehicle_id in vehicles and appointment.mechanic_id in mechanics:
         return {
-            "message": update_collection(app_id, appointment, apps),
+            "message": await update_collection(app_id, appointment, apps),
             "collection": "appointments"
         }
     else:
@@ -110,7 +117,7 @@ async def put_appointment(app_id: str, appointment: Appointment):
 @app.put("/vehicles/{vehicle_id}")
 async def put_vehicle(vehicle_id: str, vehicle: Vehicle):
     return {
-        "message": update_collection(vehicle_id, vehicle, vehicles),
+        "message": await update_collection(vehicle_id, vehicle, vehicles),
         "collection": "vehicles"
     }
 
@@ -118,18 +125,19 @@ async def put_vehicle(vehicle_id: str, vehicle: Vehicle):
 @app.put("/mechanics/{mechanic_id}")
 async def put_mechanic(mechanic_id: str, mechanic: Mechanic):
     return {
-        "message": update_collection(mechanic_id, mechanic, mechanics),
+        "message": await update_collection(mechanic_id, mechanic, mechanics),
         "collection": "mechanics"
     }
 
 
 # *************** DELETES ***************
 
-def delete_from_collection(entity_id: str, resource: Resource) -> str:
-    if entity_id in resource.entities:
-        del resource.entities[entity_id]
+async def delete_from_collection(entity_id: str, resource: Resource) -> str:
+    async with resource.mutex:
+        if entity_id in resource.entities:
+            del resource.entities[entity_id]
 
-        return f"deleted entity {entity_id}"
+            return f"deleted entity {entity_id}"
 
     return f"delete failed! Token not found {entity_id}"
 
@@ -141,7 +149,9 @@ async def delete_future_appointments():
         is_in_the_future = datetime.strptime(appointment.date, "%d/%m/%Y") >= datetime.today()
 
         if is_in_the_future:
-            apps.entities.pop(key)
+            async with apps.mutex:
+                if key in apps.entities.keys():
+                    apps.entities.pop(key)
 
     return {
         "message": "future appointments deleted",
@@ -152,7 +162,7 @@ async def delete_future_appointments():
 @app.delete("/apps/{app_id}")
 async def delete_appointment(app_id: str):
     return {
-        "message": delete_from_collection(app_id, apps),
+        "message": await delete_from_collection(app_id, apps),
         "collection": "appointments"
     }
 
@@ -160,31 +170,33 @@ async def delete_appointment(app_id: str):
 @app.delete("/vehicles/{vehicle_id}")
 async def delete_vehicle(vehicle_id: str):
     keys_to_pop = []
-    for (key, appointment) in apps.entities.items():
-        if appointment.vehicle_id == vehicle_id:
-            appointment.vehicle_id = "!!!!!"
+    async with apps.mutex:
+        for (key, appointment) in apps.entities.items():
+            if appointment.vehicle_id == vehicle_id:
+                appointment.vehicle_id = "!!!!!"
 
-            is_in_the_future = datetime.strptime(appointment.date, "%d/%m/%Y") >= datetime.today()
-            if is_in_the_future:
-                keys_to_pop.append(key)
+                is_in_the_future = datetime.strptime(appointment.date, "%d/%m/%Y") >= datetime.today()
+                if is_in_the_future:
+                    keys_to_pop.append(key)
 
-    for key in keys_to_pop:
-        apps.entities.pop(key)
+        for key in keys_to_pop:
+            apps.entities.pop(key)
 
     return {
-        "message": delete_from_collection(vehicle_id, vehicles),
+        "message": await delete_from_collection(vehicle_id, vehicles),
         "collection": "vehicles"
     }
 
 
 @app.delete("/mechanics/{mechanic_id}")
 async def delete_mechanic(mechanic_id: str):
-    for appointment in apps.entities.values():
-        if appointment.mechanic_id == mechanic_id:
-            appointment.mechanic_id = "!!!!!"
+    async with apps.mutex:
+        for appointment in apps.entities.values():
+            if appointment.mechanic_id == mechanic_id:
+                appointment.mechanic_id = "!!!!!"
 
     return {
-        "message": delete_from_collection(mechanic_id, mechanics),
+        "message": await delete_from_collection(mechanic_id, mechanics),
         "collection": "mechanics"
     }
 
@@ -193,7 +205,6 @@ async def delete_mechanic(mechanic_id: str):
 
 def is_in_collection(entity_id: str, resource: Resource) -> str:
     if entity_id in resource.entities:
-
         return f"get request successful"
 
     return f"get request failed! Token not found {entity_id}"
